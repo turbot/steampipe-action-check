b62c49dff8d73d991e6a41bf990059feac07cff2
Karan Popat <karan@turbot.com>
2022-09-14 18:12:16 +0800

    update workflow

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ async function run() {
         return
       }
     }
-    // await RunSteampipeCheck(steampipePath, modPath)
+    await RunSteampipeCheck(steampipePath, modPath)
   } catch (error) {
     setFailed(error.message);
   }
