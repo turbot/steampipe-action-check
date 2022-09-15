27b5ef152f6d3ed54b915be478f73d3fe0f56111
Karan Popat <karan@turbot.com>
2022-09-16 02:11:34 +0800

    add commenter

--- src/commenter.ts
+++ src/commenter.ts
@@ -46,7 +46,8 @@ async function CommentOnLine(actionInputs: ActionInput, result: Result) {
       line: +(splitted[1]),
       commit_id: github.context.payload.pull_request['head']['sha'],
       // path: splitted[0].replace(process.cwd(), '') //examples/terraform/aws/ec2/ec2_ebs_default_encryption_enabled.tf
-      path: splitted[0].split("/")[splitted[0].split("/").length - 1]
+      // path: splitted[0].split("/")[splitted[0].split("/").length - 1]
+      path: 'https://github.com/turbot/steampipe-action/pull/7/files#diff-1540d0669c3b13577f36bef3c817e7c60f61323750282f0e8a20f908413b5e66'
       // start_line: +(splitted[1]),
       // start_side: "RIGHT",
       // side: "RIGHT"
