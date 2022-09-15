44e3d678d707c36e3f5bac8697d8065595a19510
Binaek Sarkar <binaek@turbot.com>
2022-09-15 14:40:30 +0800

    code

--- src/steampipe.ts
+++ src/steampipe.ts
@@ -94,9 +94,7 @@ export async function InstallMod(modRepository: string) {
   }
   const cloneTo = `workspace_dir_${new Date().getTime()}`
   info(`Installing mod from ${modRepository}`)
-  await exec(await which("git", true), ["clone", modRepository, cloneTo], {
-
-  })
+  await exec(await which("git", true), ["clone", modRepository, cloneTo])
   endGroup()
   return cloneTo
 }
