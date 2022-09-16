031da35a731b51c6283a94a0fd166499bdf74554
Binaek Sarkar <binaek@turbot.com>
2022-09-16 20:03:18 +0800

    removing exports

--- tfscan/src/annotate.ts
+++ tfscan/src/annotate.ts
@@ -98,7 +98,17 @@ function getAnnotationsForControl(controlRun: ControlRun): Array<Annotation> {
   return annotations;
 }
 
-export interface Status {
+export interface Annotation {
+  path: string;
+  start_line: number;
+  end_line: number;
+  annotation_level: string;
+  message: string;
+  start_column: number;
+  end_column: number;
+}
+
+interface Status {
   alarm?: number;
   ok?: number;
   info?: number;
@@ -106,23 +116,23 @@ export interface Status {
   error?: number;
 }
 
-export interface Summary {
+interface Summary {
   status?: Status;
 }
 
-export interface Dimension {
+interface Dimension {
   key?: string;
   value?: string;
 }
 
-export interface ControlResult {
+interface ControlResult {
   reason?: string;
   resource?: string;
   status?: string;
   dimensions?: Dimension[];
 }
 
-export interface ControlRun {
+interface ControlRun {
   summary?: Status;
   results?: ControlResult[];
   controlId?: string;
@@ -134,24 +144,14 @@ export interface ControlRun {
   runError?: string;
 }
 
-export interface GroupResult {
+interface GroupResult {
   groupId?: string;
   title?: string;
   description?: string;
   tags?: string;
   summary?: Summary;
-  groups?: GroupResult[];
-  controls?: ControlRun[];
+  groups: GroupResult[] | null;
+  controls: ControlRun[] | null;
 }
 
-export type RootResult = GroupResult
-
-export interface Annotation {
-  path: string;
-  start_line: number;
-  end_line: number;
-  annotation_level: string;
-  message: string;
-  start_column: number;
-  end_column: number;
-}
+type RootResult = GroupResult
