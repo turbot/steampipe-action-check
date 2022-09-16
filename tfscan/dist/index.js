09017a19bf85e97fdc53e3b522f9e283cfba8933
Binaek Sarkar <binaek@turbot.com>
2022-09-16 18:00:00 +0800

    build

--- tfscan/dist/index.js
+++ tfscan/dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -14012,4 +14012,3 @@ run();
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
