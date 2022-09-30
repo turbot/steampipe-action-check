b1a6985e85a695bce1f1fdc377bed70080163ad8
Karan Popat <karan@turbot.com>
2022-09-30 19:42:20 +0530

    test action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,22 @@
 
 # set -xe
 
+  cat /etc/passwd
 if [ "$INPUT_STEAMPIPE_VERSION" == "latest" ]
 then
   echo "latest"
   # wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" -O - | tar -xz
   wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
+  su test
+  whoami
   ./steampipe query "select 1"
 
 else
   echo "not latest"
   # wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" -O - | tar -xz
   wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
+  su test
+  whoami
   ./steampipe query "select 1"
 fi
 
