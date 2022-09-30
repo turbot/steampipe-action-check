abdc83148e88a8af1f71e483b3fb3d046799905b
Karan Popat <karan@turbot.com>
2022-09-30 17:57:31 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ echo "Hello $INPUT_STEAMPIPE_VERSION"
 if [ "$INPUT_STEAMPIPE_VERSION" == "latest" ]
 then
   echo "latest"
+  wget "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz"
 else
   echo "not latest"
+  wget "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz"
 fi
 uname -mp
