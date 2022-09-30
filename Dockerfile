bf2bd475df8edf7bf658bc675c9da79714e646b7
Karan Popat <karan@turbot.com>
2022-09-30 20:55:09 +0530

    test action

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,27 @@
 # Container image that runs your code
 FROM alpine:3.10
 
+ARG USER=default
+ENV HOME /home/$USER
+
 # RUN apk --no-cache --update add bash git \
 #     && rm -rf /var/cache/apk/*
 
 # Add a new user "nonroot" with user id 8877
-RUN adduser -u 8877 --disabled-password nonroot
+# RUN adduser -u 8877 -D -S nonroot
 # Change to non-root privilege
 # USER nonroot
 
+# install sudo as root
+RUN apk add --update sudo
+
+# add new user
+RUN adduser -D $USER \
+        && echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$USER \
+        && chmod 0440 /etc/sudoers.d/$USER
+
+USER $USER
+WORKDIR $HOME
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
 # set -xe
-
+whoami
 if [ "$INPUT_STEAMPIPE_VERSION" == "latest" ]
 then
   echo "latest"
   # wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" -O - | tar -xz
   wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
-  su nonroot
+    # wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz"
+    # chmod 777 ./steampipe_linux_amd64.tar.gz
+    # tar -xzf  ./steampipe_linux_amd64.tar.gz -C ./
   ./steampipe query "select 1"
 
 else
   echo "not latest"
   # wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" -O - | tar -xz
   wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
-  su nonroot
+    # wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz"
+    # chmod 777 ./steampipe_linux_amd64.tar.gz
+    # tar -xzf  ./steampipe_linux_amd64.tar.gz -C ./
   whoami
   ./steampipe query "select 1"
 fi
