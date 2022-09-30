4b9e90866285c548c75da6ba1b0756483a8ad8ab
Karan Popat <karan@turbot.com>
2022-09-30 21:03:35 +0530

    test action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 # set -xe
+pwd
+cd /home/default
 whoami
 if [ "$INPUT_STEAMPIPE_VERSION" == "latest" ]
 then
   echo "latest"
   # wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" -O - | tar -xz
-  wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
+  wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C /home/default
     # wget -c "https://github.com/turbot/steampipe/releases/latest/download/steampipe_linux_amd64.tar.gz"
     # chmod 777 ./steampipe_linux_amd64.tar.gz
     # tar -xzf  ./steampipe_linux_amd64.tar.gz -C ./
-  ./steampipe query "select 1"
+  /home/default/steampipe query "select 1"
 
 else
   echo "not latest"
   # wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" -O - | tar -xz
-  wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C ./
+  wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz" && tar -xzf  steampipe_linux_amd64.tar.gz -C /home/default
     # wget -c "https://github.com/turbot/steampipe/releases/download/v0.16.4/steampipe_linux_amd64.tar.gz"
     # chmod 777 ./steampipe_linux_amd64.tar.gz
     # tar -xzf  ./steampipe_linux_amd64.tar.gz -C ./
   whoami
-  ./steampipe query "select 1"
+  /home/default/steampipe query "select 1"
 fi
 
 
