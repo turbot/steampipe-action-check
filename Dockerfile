ff4ca9a7729efd8439c51f3004d21fa5892992f7
Karan Popat <karan@turbot.com>
2022-10-03 16:53:58 +0530

    working check

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,14 @@
 FROM turbot/steampipe
 
 # Setup prerequisites (as root)
-ARG mod_url
-ARG plugin
-ARG steampipe_version
-
 USER root:0
+
 RUN apt-get update -y \
  && apt-get install -y git
+ 
 RUN echo $steampipe_version
 # Install the aws and steampipe plugins for Steampipe (as steampipe user).
 USER steampipe:0
-RUN  steampipe plugin install $plugin
-
-# A mod may be installed to a working directory
-RUN  git clone --depth 1 $mod_url /workspace
-WORKDIR /workspace
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,38 @@
 #!/bin/sh
 
-# set -xe
-echo "Done!!!!!!!"
+set -xe
 steampipe query "select 1"
 
+setup_plugin()
+{
+  steampipe plugin install $INPUT_PLUGIN
+  
+  # Add config file
+  export X=0
+  echo "\nconnection \"$INPUT_PLUGIN$X\" {" > ~/.steampipe/config/terraform.spc
+  echo "  plugin = \"$INPUT_PLUGIN\"" >> ~/.steampipe/config/terraform.spc
+  echo "  paths = [ \"/home/steampipe/**/*.tf\" ]" >> ~/.steampipe/config/terraform.spc
+  echo "}" >> ~/.steampipe/config/terraform.spc
+  cat ~/.steampipe/config/terraform.spc
+}
 
-pwd
+run_infra_check() {
+  if steampipe check $INPUT_RUN ; then
+    echo "Success"
+  else
+    echo "Fail"
+  fi
+}
 
+# Setup plugin
+setup_plugin
+
+# Copy infra files
+cp -r examples/terraform/aws /home/steampipe/aws
+
+# Mod installation
+git clone --depth 1 $INPUT_MOD_URL /workspace
+cd /workspace
+
+# Run infra check
+run_infra_check
