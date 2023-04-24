7c949e0c0cc2186cf6664a53a0a0c0108c51e0c2
Binaek Sarkar <binaek@turbot.com>
2023-04-24 16:29:30 +0530

    update indexjs

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import { processAnnotations } from "./annotate";
 
 async function run() {
   try {
-
-    token = getInput("github-token", { trimWhitespace: true })
-    run = getInput("run", { trimWhitespace: true })
-    runs = []
+    token = getInput("github-token", { trimWhitespace: true });
+    run = getInput("run", { trimWhitespace: true });
+    runs = [];
     for (let i = 2; i < process.argv.length; i++) {
       runs.push(process.argv[i]);
     }
-    await processAnnotations({ token, runs })
-
+    await processAnnotations({ token, runs });
   } catch (error) {
     setFailed(error.message);
   }
 }
 
 // run()
-console.log("Now we will annotate the PR with the JS code that is getting executed")
+console.log(
+  "Now we will annotate the PR with the JS code that is getting executed",
+  process.argv
+);
