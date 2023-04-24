813b25cf378a48e876f200e93bfbeaee8f44b646
Binaek Sarkar <binaek@turbot.com>
2023-04-24 16:41:10 +0530

    entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,68 @@
 #!/bin/sh
 
 set -xe
-steampipe query "select 1"
+
+# install steampipe
+RUN=$INPUT_RUN
+VERSION_TO_INSTALL=
+if [ -z "$INPUT_STEAMPIPE_VERSION" ]
+then
+  echo ""
+else
+  VERSION_TO_INSTALL=$INPUT_STEAMPIPE_VERSION
+fi
+
+curl https://raw.githubusercontent.com/turbot/steampipe/main/install.sh -o /tmp/install.sh
+chmod +x /tmp/install.sh 
+/tmp/install.sh $VERSION_TO_INSTALL
+
+SP=$(which steampipe)
+
+INSTALL_DIR=/install
+PLUGIN_NAME=terraform
+
+RunList=()
+if [ -z "$RUN" ];then
+  RunList=("all")
+else
+  while read -r line; do
+    RunList+=("$line")
+  done <<< "$RUN"
+fi
+
+$SP query "select 1" --install-dir $INSTALL_DIR
 
 setup_plugin()
 {
-  steampipe plugin install $INPUT_PLUGIN
+  $SP plugin install $PLUGIN_NAME --install-dir $INSTALL_DIR
   
+  connection_data="
+connection \"terraform\" {
+  plugin = \"terraform\"
+  paths = [ \"$INPUT_PATHS/**/*.tf\" ]
+}
+"
+
   # Add config file
-  export X=0
-  echo "\nconnection \"$INPUT_PLUGIN$X\" {" > /home/steampipe/.steampipe/config/terraform.spc
-  echo "  plugin = \"$INPUT_PLUGIN\"" >> /home/steampipe/.steampipe/config/terraform.spc
-  echo "  paths = [ \"/home/steampipe/**/*.tf\" ]" >> /home/steampipe/.steampipe/config/terraform.spc
-  echo "}" >> /home/steampipe/.steampipe/config/terraform.spc
-  cat /home/steampipe/.steampipe/config/terraform.spc
+  echo $connection_data >> $INSTALL_DIR/config/terraform.spc
+  echo "Wrote connection file:"
+  cat $INSTALL_DIR/config/terraform.spc
+  echo "<<<<<<<<<<<<<<<<<<<<<<"
 }
 
 run_infra_check() {
-  if steampipe check $INPUT_RUN --export=check.json; then
-    echo "Success"
+  if $SP check ${RunList[@]} --export=json --mod-location=/workspace; then
+    echo "S"
   else
-    echo "Fail"
+    echo "F"
   fi
 }
 
-# Setup plugin
 setup_plugin
-
-# Copy infra files
-pwd
-cp -r examples/terraform/aws /home/steampipe/aws
 
 # Mod installation
 git clone --depth 1 $INPUT_MOD_URL /workspace
-cd /workspace
 
-# Run infra check
 run_infra_check
+
+node /js-app/index.js benchmark.b1 benchmark.b2
