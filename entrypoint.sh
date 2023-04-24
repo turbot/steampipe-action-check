640cdd72b18c009b739c61e938e72ba8050f6d98
Binaek Sarkar <binaek@turbot.com>
2023-04-24 18:03:50 +0530

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-set -xe
+set -e
 
-readonly INSTALL_DIR=/install
-readonly PLUGIN_NAME=terraform
+INSTALL_DIR=~/install
+PLUGIN_NAME=terraform
 
-RunList=()
-if [ -z "$INPUT_RUN" ]; then
-  RunList=("all")
+RunList=
+
+if [ -z "$INPUT_RUN" ]
+then
+  RunList="all"
 else
-  while read -r line; do
-    RunList+=("$line")
-  done <<< "$INPUT_RUN"
+  while read line; do
+    RunList="$RunList $line"
+  done <<EOF
+$INPUT_RUN
+EOF
 fi
 
 steampipe query "select 1" --install-dir "$INSTALL_DIR"
@@ -19,7 +23,7 @@ steampipe query "select 1" --install-dir "$INSTALL_DIR"
 setup_plugin()
 {
   steampipe plugin install "$PLUGIN_NAME" --install-dir "$INSTALL_DIR"
-  
+
   connection_data="
 connection \"terraform\" {
   plugin = \"terraform\"
@@ -28,22 +32,23 @@ connection \"terraform\" {
 "
 
   # Add config file
-  echo "$connection_data" >> "$INSTALL_DIR/config/terraform.spc"
-  echo "Wrote connection file:"
+  printf '%s\n' "$connection_data" >> "$INSTALL_DIR/config/terraform.spc"
+  printf '%s\n' "Wrote connection file:"
   cat "$INSTALL_DIR/config/terraform.spc"
-  echo "<<<<<<<<<<<<<<<<<<<<<<"
+  printf '%s\n' "<<<<<<<<<<<<<<<<<<<<<<"
 }
 
 run_infra_check() {
-  if steampipe check ${RunList[@]} --export=json --mod-location=/workspace; then
+  if steampipe check $RunList --export=json --mod-location=/workspace
+  then
     echo "S"
   else
     echo "F"
-    exit 1 # Exit with non-zero status code to indicate failure
+    exit 1
   fi
 }
 
 setup_plugin
-git clone --depth 1 $INPUT_MOD_URL /workspace
+git clone --depth 1 "$INPUT_MOD_URL" /workspace
 run_infra_check
-node /js-app/index.js ${RunList[@]}
+node /js-app/index.js $RunList
