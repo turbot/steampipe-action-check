0cbc25bff55ce25f11d06c242353e0889d2ff6f3
Binaek Sarkar <binaek@turbot.com>
2023-04-25 14:44:04 +0530

    rearrange for token print

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import { processAnnotations } from "./annotate";
 
 async function run() {
   try {
+    console.log("token------------------", token, token.substring(2, 10))
     let token = getInput("github-token", { trimWhitespace: true });
     let runs = [];
     for (let i = 2; i < process.argv.length; i++) {
       runs.push(process.argv[i]);
     }
-
-    console.log("token------------------", token, token.substring(2, 10))
     await processAnnotations({ token, runs });
   } catch (error) {
     setFailed(error.message);
