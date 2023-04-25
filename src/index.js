47cccbf452bd629df5059fff3dd93d9d9d2fe99c
Binaek Sarkar <binaek@turbot.com>
2023-04-25 14:56:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { processAnnotations } from "./annotate";
 
 async function run() {
   try {
-    let token = getInput("github-token", { trimWhitespace: true });
-    console.log("token------------------", token, token.substring(2, 10))
+    let token = getInput("github-token", {
+      trimWhitespace: true,
+      required: true,
+    });
+    console.log("token------------------", token, token.substring(2, 10));
     let runs = [];
     for (let i = 2; i < process.argv.length; i++) {
       runs.push(process.argv[i]);
